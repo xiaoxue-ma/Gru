382bd852f61b6d93821c3467d0ffb9cc1a5898b8
elevenloveseleven <elevenloveseleven@hotmail.com>
2015-09-29 19:34:11 +0800

    like and unlike comment; new moment button

--- SeniorConnect/www/js/moments.js
+++ SeniorConnect/www/js/moments.js
@@ -3,15 +3,38 @@
  */
 angular.module('sc.moments',[])
   .controller('MomentsCtrl', function($scope){
+    $scope.now =0;
 
-    function loadFeeds() {
-      $scope.feeds = Status.query({user_id: $stateParams.friendId});
+    $scope.isliked = function(){
+      return $scope.now;
+    };
+    $scope.like = function(){
+      $scope.now = 1;
+    };
+    $scope.dislike = function(){
+      $scope.now = 0;
+    };
 
-      $scope.likedItemsLocal = [];
-      $scope.currentItem = 0;
-      $scope.currentDisplayPane = 0;
-      $ionicLoading.hide();
-      $scope.$broadcast('scroll.refreshComplete');
-    }
-
+    $scope.newPost = function () {
+      $ionicActionSheet.show({
+        buttons: [
+          {text: 'social.take_photo'},
+          {text: 'social.select_photos_from_phone'},
+          {text: 'social.text_only'}
+        ],
+        titleText: 'social.create_new_post',
+        buttonClicked: function (index) {
+          if (index == 0) {
+            $state.go('single-page.social-new-post-new-photo');
+          }
+          if (index == 1) {
+            $state.go('single-page.social-new-post-upload-photo');
+          }
+          if (index == 2) {
+            $state.go('single-page.social-new-post-text-only');
+          }
+          return true;
+        }
+      });
+    };
   });
