d1c16d24ecbc95800e4e1ee49cff0fd198bbcddf
elevenloveseleven <elevenloveseleven@hotmail.com>
2015-10-13 13:11:34 +0800

    remove old files

--- SeniorConnect/Old/www/js/profile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Boss on 25/9/15.
- */
-angular.module('sc.profile',[])
-  .controller('ProfileCtrl', function($scope) {
-    $scope.settings = {
-      enableFriends: true
-    };
-  });
